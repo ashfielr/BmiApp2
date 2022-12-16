a571c04f359ee334b74578e44155bf0dcb519456
Robert Ashfield <ashfielr@uni.coventry.ac.uk>
2022-12-16 10:24:51 +0000

    basic bmi calculation view implemented

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+// creating component which deals with BMI calculation, props = properties for the component
+// entering weight and height and then clicking button will show BMI
+const BMICalculatorComponent = (props) => {
+  // Declare state variables
+  const [weight, onWeightChange] = React.useState(0) //  [<getter>,<setter>]  for state variable
+  // useState sets initial value
+  const [height, onHeightChange] = React.useState(0)
+  const [bmi, setBmi] = React.useState(0)
+  return (
+    <SafeAreaView>
+      <Text style={styles.textInputLabel}>Weight (Kg)</Text>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={onWeightChange}
+        value={weight}
+        keyboardType="default"
+      />
+      <Text style={styles.textInputLabel}>Height (m)</Text>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={onHeightChange}
+        value={height}
+        keyboardType="default"
+      />
+      <Button
+        onPress={() => {
+          const bmiVal = weight/Math.pow(height,2)
+          setBmi(Math.round((bmiVal + Number.EPSILON)*100)/100) // *100 and /100 to round to 2 d.p.
+        }}
+        title="Calculate BMI"
+        accessibilityLabel="Calculate the BMI" />
+        <Text>BMI: {bmi}</Text>
+    </SafeAreaView>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <BMICalculatorComponent />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +53,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textInputLabel: {
+    fontSize: 24,
+    fontweight: 'bold',
+  },
+  textInput: {
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 10,
+    margin: 12,
   },
 });
